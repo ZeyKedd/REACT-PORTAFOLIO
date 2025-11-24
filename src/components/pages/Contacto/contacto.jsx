6ed5e14dd6eb39811bda6e7be4aa3764fc7a2783
Sebastian Hernandez <sebhrz08@gmail.com>
2025-11-24 16:30:15 -0500

    Añadir contenido de contacto

--- src/components/pages/Contacto/contacto.jsx
+++ src/components/pages/Contacto/contacto.jsx
@@ -1,8 +1,18 @@
 import React from 'react'
+import './contacto.style.css'
+import foto from '../../img/foto-recortada.png'
 
 export const contacto = () => {
     return (
-        <div>contacto</div>
+        <div>
+            <div class="card text-bg-dark " style={{width: "40rem"}}>
+                <img src= {foto} class="circle-img " alt="..."/>
+                    <h1 class="text-circle-image">SEBASTIAN HERNANDEZ</h1>
+                    <h6 class="text-circle-image">CALI - COLOMBIA</h6>
+                    <div class="card-body">
+                    </div>
+            </div>
+        </div>
     )
 }
 
