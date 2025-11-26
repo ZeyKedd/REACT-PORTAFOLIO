6606c8264677ab49dfc6eb573ec695bbddd396ad
Sebastian Hernandez <sebhrz08@gmail.com>
2025-11-26 15:40:08 -0500

    Se agrega botones en contactos

--- src/components/pages/Contacto/contacto.jsx
+++ src/components/pages/Contacto/contacto.jsx
@@ -1,6 +1,9 @@
 import React from 'react'
 import './contacto.style.css'
 import foto from '../../img/foto-recortada.png'
+import { TbBrandGmail } from "react-icons/tb";
+import { FaGithub } from "react-icons/fa6";
+import { FaLinkedin } from "react-icons/fa";
 
 export const contacto = () => {
     return (
@@ -10,6 +13,9 @@ export const contacto = () => {
                     <h1 class="text-circle-image">SEBASTIAN HERNANDEZ</h1>
                     <h6 class="text-circle-image">CALI - COLOMBIA</h6>
                     <div class="card-body">
+                        <a title='Github' class="iconsContact" href="https://github.com/ZeyKedd" target="_blank" rel="noopener noreferrer"> <FaGithub/> </a>
+                        <a title='Correo' class="iconsContact" href="mailto:sebhrz08@gmail.com"> <TbBrandGmail /></a>
+                        <a title='Linkedin' class="iconsContact" href="https://www.linkedin.com/in/sebastian-hernandez-581b87340/?trk=opento_sprofile_goalscard" target="_blank" rel="noopener noreferrer"> <FaLinkedin /></a>
                     </div>
             </div>
         </div>
